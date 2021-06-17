38024f42dba874356038cf5912827e8478d92695
barsikus007 <37113583+barsikus007@users.noreply.github.com>
2021-06-17 14:19:09 +0300

    create_db refactor

--- create_db.py
+++ create_db.py
@@ -1,128 +1,41 @@
 import csv
-import pickle
 import asyncio
+from datetime import datetime
 
-import asyncpg
 import requests
-import sqlalchemy
+from asyncpg.exceptions import ConnectionDoesNotExistError
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import text
 
 from config import get_students_url, DB_AUTH
 from utils import Base, engine, async_session
 from dal import StudentsDAL, EventsDAL, CalendarDAL
 
 
-async def connect_or_create():
-    import csv
-    engine2 = create_async_engine(f'postgresql+asyncpg://{DB_AUTH["user"]}:{DB_AUTH["password"]}@{DB_AUTH["host"]}:{DB_AUTH["port"]}/test', echo=False)
-    async_session2 = sessionmaker(engine2, expire_on_commit=False, class_=AsyncSession)
-
-    async with engine2.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-    students_list = requests.get(get_students_url).json()['data']['allStudent']
-    students_list = [[_['studentID'], _['fullName']] for _ in students_list]
-    async with async_session2() as session:
-        async with session.begin():
-            students = StudentsDAL(session)
-            await students.create_students(students_list)
-    with open('csv/students.csv', 'r', encoding='UTF-8') as f:
-        in_csv = csv.reader(f)
-        async with async_session2() as session:
-            async with session.begin():
-                students = StudentsDAL(session)
-                for row in in_csv:
-                    if in_csv.line_num == 1:
-                        continue
-                    if not row:
-                        continue
-                    if not(row[2] or row[3]):
-                        continue
-                    if row[2]:
-                        tg_id = int(row[2])
-                    else:
-                        tg_id = None
-                    if row[3]:
-                        cal_id = row[3]
-                    else:
-                        cal_id = None
-                    await students.update_student(student_id=int(row[0]), telegram_id=tg_id, calendar_id=cal_id)
-    with open('csv/events.csv', 'r', encoding='UTF-8') as f:
-        in_csv = csv.reader(f)
-        async with async_session2() as session:
-            async with session.begin():
-                events = EventsDAL(session)
-                for row in in_csv:
-                    if in_csv.line_num == 1:
-                        continue
-                    if not row:
-                        continue
-                    print(row)
-                    continue
-                    if not(row[2] or row[3]):
-                        continue
-                    if row[2]:
-                        tg_id = int(row[2])
-                    else:
-                        tg_id = None
-                    if row[3]:
-                        cal_id = row[3]
-                    else:
-                        cal_id = None
-                    await events.update_student(student_id=int(row[0]), telegram_id=tg_id, calendar_id=cal_id)
-    exit()
-    with open('csv/calendar.csv', 'r', encoding='UTF-8') as f:
-        in_csv = csv.reader(f)
-        async with async_session2() as session:
-            async with session.begin():
-                calendar = CalendarDAL(session)
-                for row in in_csv:
-                    if in_csv.line_num == 1:
-                        continue
-                    if not row:
-                        continue
-                    print(row)
-                    continue
-                    if not(row[2] or row[3]):
-                        continue
-                    if row[2]:
-                        tg_id = int(row[2])
-                    else:
-                        tg_id = None
-                    if row[3]:
-                        cal_id = row[3]
-                    else:
-                        cal_id = None
-                    await students.update_student(student_id=int(row[0]), telegram_id=tg_id, calendar_id=cal_id)
-    exit()
-    try:
-        conn = await asyncpg.connect(**DB_AUTH)
-    except (asyncpg.exceptions.ConnectionDoesNotExistError, asyncpg.InvalidCatalogNameError):
-        sys_conn = await asyncpg.connect(
-            database='template1',
-            user=DB_AUTH['user'],
-            host=DB_AUTH['host'],
-            port=DB_AUTH['port'],
-            password=DB_AUTH['password'],
-        )
-        user = DB_AUTH['user']
-        database = DB_AUTH['database']
-        await sys_conn.execute(
-            f'CREATE DATABASE "{database}" OWNER "{user}"'
-        )
-        await sys_conn.close()
-        conn = await asyncpg.connect(**DB_AUTH)
-    return conn
+async def create_if_not_exists():
+    postgres_engine = create_async_engine(
+        f'postgresql+asyncpg://{DB_AUTH["user"]}:{DB_AUTH["password"]}@{DB_AUTH["host"]}:{DB_AUTH["port"]}/postgres',
+        echo=False
+    )
+    postgres_async_session = sessionmaker(postgres_engine, expire_on_commit=False, class_=AsyncSession)
+    async with postgres_async_session.begin() as session:
+        await session.execute('COMMIT')
+        await session.execute(f'CREATE DATABASE {DB_AUTH["database"]}')
 
 
 async def reset():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+async def create_or_connect_and_reset():
+    try:
+        await reset()
+    except ConnectionDoesNotExistError:
+        await create_if_not_exists()
+        await reset()
 
 
 async def put_students_from_site():
@@ -135,7 +48,6 @@ async def put_students_from_site():
 
 
 async def add_data_from_old_db():
-    exit('TODO csv')
     with open('csv/students.csv', 'r', encoding='UTF-8') as f:
         in_csv = csv.reader(f)
         async with async_session() as session:
@@ -146,22 +58,59 @@ async def add_data_from_old_db():
                         continue
                     if not row:
                         continue
-                    if not(row[2] or row[3]):
+                    if not (row[2] or row[3]):
                         continue
-                    if row[2]:
-                        tg_id = int(row[2])
-                    else:
-                        tg_id = None
-                    if row[3]:
-                        cal_id = row[3]
-                    else:
-                        cal_id = None
-                    await students.update_student(student_id=int(row[0]), telegram_id=tg_id, calendar_id=cal_id)
+                    await students.update_student(
+                        student_id=int(row[0]),
+                        telegram_id=int(row[2]) if row[2] else None,
+                        calendar_id=row[3] if row[3] else None
+                    )
+    with open('csv/events.csv', 'r', encoding='UTF-8') as f:
+        in_csv = csv.reader(f)
+        async with async_session() as session:
+            async with session.begin():
+                events = EventsDAL(session)
+                for row in in_csv:
+                    if in_csv.line_num == 1:
+                        continue
+                    if not row:
+                        continue
+                    await events.create_event(
+                        {
+                            'rasp_item_id': int(row[0]),
+                            'start': datetime.strptime(row[1], '%Y-%m-%d %H:%M:%S%z'),
+                            'end': datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S%z'),
+                            'name': row[3],
+                            'color': row[4] if row[4] else None,
+                            'aud': row[5] if row[5] else None,
+                            'link': row[6] if row[6] else None,
+                            'teacher': row[7] if row[7] else None,
+                            'module_name': row[8] if row[8] else None,
+                            'theme': row[9] if row[9] else None,
+                            'group_name': row[10] if row[10] else None,
+                            'description': row[11] if row[11] else None,
+                            'hash': row[12] if row[12] else None
+                        }
+                    )
+    with open('csv/calendar.csv', 'r', encoding='UTF-8') as f:
+        in_csv = csv.reader(f)
+        async with async_session() as session:
+            async with session.begin():
+                calendar = CalendarDAL(session)
+                for row in in_csv:
+                    if in_csv.line_num == 1:
+                        continue
+                    if not row:
+                        continue
+                    await calendar.create_calendar(
+                        student_id=int(row[0]),
+                        rasp_item_id=int(row[1]),
+                        event_hash=row[2]
+                    )
 
 
 if __name__ == '__main__':
     exit('Locked, use with care!')
-    asyncio.get_event_loop().run_until_complete(connect_or_create())
-    asyncio.get_event_loop().run_until_complete(reset())
+    asyncio.get_event_loop().run_until_complete(create_or_connect_and_reset())
     asyncio.get_event_loop().run_until_complete(put_students_from_site())
     asyncio.get_event_loop().run_until_complete(add_data_from_old_db())
